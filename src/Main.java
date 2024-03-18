22e8cdbed263311a2469a302fa44c2c632ddb6e3
REIGaruki <gyakujo@mail.ru>
2024-03-18 15:08:33 +0300

    Create classes

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,20 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    public class Book {
+        private String bookName;
+        int publishYear;
+        private Author author;
+        public Book(String bookName, Author author, int publishYear) {
+            this. bookName = bookName;
+            this.author = author;
+            this. publishYear = publishYear;
+        }
+    }
+    public class Author{
+        String authorName;
+        public Author(String authorName) {
+            this.authorName = authorName;
+        }
+    }
 }
