17d239ede69b13da16bcc36cce3086d51860983c
REIGaruki <gyakujo@mail.ru>
2024-03-18 15:13:13 +0300

    Getters & setters

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,33 @@ public class Main {
             this.author = author;
             this. publishYear = publishYear;
         }
+        public Book(String bookName, Author author) {
+            this. bookName = bookName;
+            this.author = author;
+        }
+        public String getBookName() {
+            return this.bookName;
+        }
+
+        public Author getAuthor() {
+            return author;
+        }
+
+        public int getPublishYear() {
+            return publishYear;
+        }
+
+        public void setPublishYear(int publishYear) {
+            this.publishYear = publishYear;
+        }
     }
     public class Author{
         String authorName;
         public Author(String authorName) {
             this.authorName = authorName;
+        }
+        public String getAuthorName() {
+            return this.authorName;
         }
     }
 }
